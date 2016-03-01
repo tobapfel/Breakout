2c36e9cb37474ddc610f90c042400ebe6f6474f1
Matthias <johannsen.matthias@web.de>
2016-03-01 12:34:19 +0100

    Test Views.java

--- src/program/practicum/breakout/View.java
+++ src/program/practicum/breakout/View.java
@@ -7,11 +7,12 @@ public class View extends GraphicsProgram {
 	@Override
 	public void run() {
 		setSize(500, 700);
+		// Das hier ist mein ganz persönlicher Kommentar.
+		// Nicht löschen, bitte!
 
 	}
 
 	public Bricks addBrick (int row, col){
-		
 	}
 
 }
