6d87f0047c51e875f0b14f97e4df8d31ceda6e80
Matthias <johannsen.matthias@web.de>
2016-03-01 13:53:02 +0100

    Refactoring Paddle.java

--- src/program/practicum/breakout/Paddle.java
+++ src/program/practicum/breakout/Paddle.java
@@ -4,11 +4,8 @@ import java.awt.Color;
 import acm.graphics.GRect;
 
 public class Paddle extends GRect {
-	
-	/**
-	 * The position of the paddle in the y-direction
-	 */
-	public final int YCOORDINATE = 100;
+
+	private final int Y = 100;
 	/**
 	 * A Paddle for playing breakout
 	 * @param width The width of the paddle
