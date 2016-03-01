e921349161bc4e89d256d48196fe144a27c962f4
tobapfel <tobapfel@hotmail.de>
2016-03-01 11:21:58 +0100

    Klassen view und Bricks hinzugefügt

---
+++ src/program/practicum/breakout/Bricks.java
@@ -0,0 +1,23 @@
+package program.practicum.breakout;
+
+import acm.graphics.GRect;
+
+public class Bricks extends GRect {
+
+	public Bricks(int row, int col) {
+		super(col * 10 + row * 2, 50 + row * 5 + (row - 1) * 2);
+		
+		switch(col){
+		case 1:
+		case 2:
+			GRect.
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		}
+	}
+
+}
---
+++ src/program/practicum/breakout/View.java
@@ -0,0 +1,13 @@
+package program.practicum.breakout;
+
+import acm.program.GraphicsProgram;
+
+public class View extends GraphicsProgram {
+
+	@Override
+	public void run() {
+		setSize(500, 700);
+		test tst = new test();
+	}
+
+}
